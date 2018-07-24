a2d542cab9e94f3625b82d7820f1b66f293fbd87
Carlos Hernández Gil <chernandezgil@gmail.com>
2018-07-24 16:35:38 +0200

    feat: Add link to frogger game

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-const element = document.querySelector('.memory-game');
-element.onclick = handleMemoryGame;
+const elementMemoryGame = document.querySelector('.memory-game');
+elementMemoryGame.onclick = handleMemoryGame;
+
+const elementFrogger = document.querySelector('.arcade-game');
+elementFrogger.onclick = handleArcadeGame;
 
 
 function handleMemoryGame(){
     window.open('https://cahergil.github.io/memorygame/');
+}
+
+function handleArcadeGame() {
+    window.open('https://cahergil.github.io/froggergame/');
 }
